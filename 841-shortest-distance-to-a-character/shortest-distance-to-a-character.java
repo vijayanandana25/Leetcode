0bbbcdd9dc8485710a0a5506232b6a8d02f8c9ae
Vijaya Nandana M <vijaya.nandana25@gmail.com>
2026-03-23 10:44:21 +0530

    Time: 1 ms (99.77%) | Memory: 44.1 MB (62.11%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+        int n = s.length();
+        int pos = -n;
+        int[] res = new int[n];
+        for(int i=0;i<n;++i){
+            if(s.charAt(i)==c){
+                pos =  i;
+            }
+            res[i]=i-pos;
+        }
+        for(int i=pos-1;i>=0;--i){
+            if(s.charAt(i)==c) pos = i;
+            res[i]= Math.min(res[i],pos-i);
+        }
+        return res;
+        
+
+    }
+}
