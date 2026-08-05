d8585dea9ce8f9eff4ac832e98cee11300bd0e5e
Vijaya Nandana M <vijaya.nandana25@gmail.com>
2026-08-05 19:23:18 +0530

    Time: 133 ms (60.14%) | Memory: 42.6 MB (95.25%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(dfs(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board,String word,int row,int col,int index){
+        if(index==word.length()) return true;
+        if(row<0||col<0||row>=board.length||col>=board[0].length) return false;
+        if(board[row][col]!=word.charAt(index)) return false;
+        char temp = board[row][col];
+        board[row][col]='#';
+        boolean found = dfs(board,word,row+1,col,index+1) || dfs(board,word,row-1,col,index+1) || dfs(board,word,row,col+1,index+1) || dfs(board,word,row,col-1,index+1);
+        board[row][col]=temp;
+        return found;
+
+    }
+}
