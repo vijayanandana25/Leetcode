8aabd48c0aae641d69ee2a6b43763eba75c1a7f6
Vijaya Nandana M <vijaya.nandana25@gmail.com>
2026-08-19 11:38:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (90.29%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==1) return nums[0];
-        return Math.max(roblinear(nums,0,n-2),roblinear(nums,1,n-1));
-
+        if (n == 1) return nums[0];
+        return Math.max(roblinear(0, n - 2, nums), roblinear(1, n - 1, nums));
     }
 
-    public int roblinear(int[] nums, int start, int end){
-        int n = end-start+1;
-        if(start==end) return nums[start];
-        //we use memoization instead of tabulation here becoz of space complexity
-        int prev2 =nums[start];
-        int prev1 =Math.max(nums[start],nums[start+1]);
-        for(int i=start+2;i<=end;i++){
-            int curr=Math.max(nums[i]+prev2,prev1);
-            prev2 =  prev1;
-            prev1=curr;
+    private static int roblinear(int start, int end, int[] nums) {
+        if (start > end) return 0;
+        if (start == end) return nums[start];
+
+        int prev2 = nums[start];
+        int prev1 = Math.max(nums[start], nums[start + 1]);
+
+        for (int i = start + 2; i <= end; i++) {
+            int curr = Math.max(prev2 + nums[i], prev1);
+            prev2 = prev1;
+            prev1 = curr;
         }
         return prev1;
     }
